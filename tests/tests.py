de934499ba94981bd9cfa5ec474ee4fce69d850f
Julio Briones <Julio_Briones@epam.com>
2022-01-02 11:23:01 -0600

    Refactored tests to be independent of each other and fail safe.

--- tests/tests.py
+++ tests/tests.py
@@ -8,20 +8,24 @@ from pathlib import Path
 def test_list_empty_dir():
     """Test -ls in an empty folder, then rm dir."""
     fldr_path = "/Users/julio_briones/Documents/Dev/Python/Core/pytest2/test_folder"
-    os.mkdir(fldr_path)
-    result = subprocess.run(["ls", fldr_path], stdout=subprocess.PIPE)
-    
-    assert not result.stdout
+    try:
+        os.mkdir(fldr_path)
+        result = subprocess.run(["ls", fldr_path], stdout=subprocess.PIPE)
 
+        assert not result.stdout, "Listing an empty folder did not return expected results."
+    finally:
+        shutil.rmtree(fldr_path)
 
 
 def test_simple_ls():
     """Test -ls, with file exists."""
-    file_path = "/Users/julio_briones/Documents/Dev/Python/Core/pytest2/test_folder/first.txt"
-    Path(file_path).touch()
-    result = subprocess.run(["ls", file_path], stdout=subprocess.PIPE)
-
-    assert result.stdout
-    
     fldr_path = "/Users/julio_briones/Documents/Dev/Python/Core/pytest2/test_folder"
-    shutil.rmtree(fldr_path, ignore_errors=True)
+    try:
+        file_path = "/Users/julio_briones/Documents/Dev/Python/Core/pytest2/test_folder/first.txt"
+        os.mkdir(fldr_path)
+        Path(file_path).touch()
+        result = subprocess.run(["ls", file_path], stdout=subprocess.PIPE)
+
+        assert "first.txt" in str(result.stdout), "Listing a dir with one file didn't return expected results."
+    finally:
+        shutil.rmtree(fldr_path, ignore_errors=True)
