a99dfc06e91c6ba9824414092470d80e3ddb6724
Julio Briones <Julio_Briones@epam.com>
2022-01-02 11:55:21 -0600

    Added listing tests for hidden files.

--- tests/tests.py
+++ tests/tests.py
@@ -32,3 +32,59 @@ class ListingTest(object):
             assert "first.txt" in str(result.stdout), "Listing a dir with one file didn't return expected results."
         finally:
             shutil.rmtree(fldr_path, ignore_errors=True)
+
+    @staticmethod
+    def test_ls_multiple_files():
+        """Test ls with multiple files exists."""
+        fldr_path = "/Users/julio_briones/Documents/Dev/Python/Core/pytest2/test_folder"
+        try:
+            file1 = "/first.txt"
+            file2 = "/second.doc"
+            os.mkdir(fldr_path)
+            Path(fldr_path + file1).touch()
+            Path(fldr_path + file2).touch()
+            result = subprocess.run(["ls", fldr_path], stdout=subprocess.PIPE)
+            print(f"Result: [{result}]")
+
+            assert "first.txt" in str(result.stdout), "Listing a dir multiple files didn't return expected results."
+            assert "second.doc" in str(result.stdout), "Listing a dir multiple files didn't return expected results."
+        finally:
+            shutil.rmtree(fldr_path, ignore_errors=True)
+
+    @staticmethod
+    def test_not_ls_a_hidden_file():
+        """Test not listing for a hidden file."""
+        fldr_path = "/Users/julio_briones/Documents/Dev/Python/Core/pytest2/test_folder"
+        try:
+            file1 = "/first.txt"
+            hidden_file = "/.hidden.txt"
+            os.mkdir(fldr_path)
+            Path(fldr_path + file1).touch()
+            Path(fldr_path + hidden_file).touch()
+            result = subprocess.run(["ls", fldr_path], stdout=subprocess.PIPE)
+            print(f"Result: [{result}]")
+
+            assert "first.txt" in str(result.stdout), "Listing a dir with one file didn't return expected results."
+            # I guess hidden files do not show in .stdout, since test passes this way.
+            assert ".hidden.txt" not in str(result.stdout), "ls a hidden file when it shouldn't have any."
+        finally:
+            shutil.rmtree(fldr_path, ignore_errors=True)
+
+    @staticmethod
+    def test_ls_a_hidden_file():
+        """Test listing for a hidden file."""
+        fldr_path = "/Users/julio_briones/Documents/Dev/Python/Core/pytest2/test_folder"
+        try:
+            file1 = "/first.txt"
+            hidden_file = "/.hidden.txt"
+            os.mkdir(fldr_path)
+            Path(fldr_path + file1).touch()
+            Path(fldr_path + hidden_file).touch()
+            result = subprocess.run(["ls", "-a", fldr_path], stdout=subprocess.PIPE)
+            print(f"Result: [{result}]")
+
+            assert "first.txt" in str(result.stdout), "Listing a dir with one file didn't return expected results."
+            # I guess hidden files do not show in .stdout, since test passes this way.
+            assert ".hidden.txt" in str(result.stdout), "ls a hidden file when it shouldn't have any."
+        finally:
+            shutil.rmtree(fldr_path, ignore_errors=True)
